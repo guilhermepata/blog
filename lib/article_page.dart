dc5967fa3f06c59b49a5b6b14e44f3679e289f5e
guilhermepata <pataguilherme@gmail.com>
2021-02-21 02:28:20 +0000

    Removed color palettes (calculations were too slow, compute did not help)

--- lib/article_page.dart
+++ lib/article_page.dart
@@ -1,8 +1,9 @@
-import 'dart:ffi';
-
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:palette_generator/palette_generator.dart';
+import 'package:icon_shadow/icon_shadow.dart';
 import 'classes.dart';
 
 class ArticlePage extends StatefulWidget {
@@ -35,10 +36,9 @@ class _ArticlePageState extends State<ArticlePage>
   ScrollController scrollController = ScrollController();
   AnimationController appBarStateController;
   Animation<Color> appBarColor;
+  Animation<Color> appBarForegroundColor;
 
-  Color dominantColor;
-  Color lightVibrantColor;
-  Color darkVibrantColor;
+  Future<String> testFuture;
 
   bool get displayMobileLayout {
     return isMobileLayout;
@@ -70,23 +70,12 @@ class _ArticlePageState extends State<ArticlePage>
       setState(() {});
     });
 
-    generatePalette();
+    testFuture = Future.delayed(
+      Duration(seconds: 2),
+      () => 'Large Latte',
+    );
 
     super.initState();
-  }
-
-  void generatePalette() async {
-    PaletteGenerator generator = await PaletteGenerator.fromImageProvider(
-        NetworkImage(widget.article.imageUrl),
-        size: Size(200, 100));
-    dominantColor =
-        generator.dominantColor != null ? generator.dominantColor.color : null;
-    lightVibrantColor = generator.lightVibrantColor != null
-        ? generator.lightVibrantColor.color
-        : null;
-    darkVibrantColor = generator.darkVibrantColor != null
-        ? generator.darkVibrantColor.color
-        : null;
   }
 
   void buildState(BuildContext context) {
@@ -139,6 +128,13 @@ class _ArticlePageState extends State<ArticlePage>
         appBarColor.addListener(() {
           setState(() {});
         });
+        appBarForegroundColor = ColorTween(
+                begin: Colors.white,
+                end: Theme.of(context).appBarTheme.foregroundColor)
+            .animate(appBarStateController);
+        appBarForegroundColor.addListener(() {
+          setState(() {});
+        });
       }
 
       isInitialized = true;
@@ -148,161 +144,214 @@ class _ArticlePageState extends State<ArticlePage>
   @override
   Widget build(BuildContext context) {
     buildState(context);
-    final originalThemeData = Theme.of(context);
 
-    var children2 =
-        widget.article.buildParagraphs(context, textAlign: TextAlign.left);
-    children2.add(ButtonBar(
-      children: [ElevatedButton(onPressed: () {}, child: Text('Button'))],
-    ));
-
-    return Theme(
-      data: originalThemeData.copyWith(
-          accentColor: darkVibrantColor, primaryColor: lightVibrantColor),
-      child: Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          // elevation: appBarStateController.value * 4,
-          shadowColor: Colors.black.withOpacity(appBarStateController.value),
-          backgroundColor: appBarColor.value,
-          leadingWidth: 56 + appBarMargins,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          title: Text(
-            widget.article.title,
-            overflow: TextOverflow.fade,
-            style: Theme.of(context).textTheme.headline6.copyWith(
-                color: Theme.of(context)
-                    .colorScheme
-                    .onSurface
-                    .withOpacity(appBarStateController.value * .87)),
-          ),
-        ),
-        body: NotificationListener(
-          onNotification: (notification) {
-            if (notification is ScrollUpdateNotification)
-              setState(
-                  () => imageScrollPosition -= notification.scrollDelta / 4);
-          },
-          child: Stack(
-            children: [
-              Positioned(
-                top: imageScrollPosition,
-                child: Padding(
-                  padding: EdgeInsets
-                      .zero, //EdgeInsets.only(left: margins, right: margins),
-                  child: Image.network(
-                    widget.article.imageUrl,
-                    fit: BoxFit.cover,
-                    alignment: Alignment.center,
-                    width: usefulWidth,
-                    // height: height,
-                    semanticLabel: widget.article.altText,
+    return FutureBuilder(
+        future: widget.article.load(),
+        builder: (context, snapshot) {
+          if (!widget.article.isLoaded)
+            return Scaffold(
+                appBar: AppBar(
+                  // elevation: appBarStateController.value * 4,
+                  shadowColor: Colors.black.withOpacity(
+                      appBarStateController.value *
+                          appBarStateController.value),
+                  backgroundColor: appBarColor.value,
+                  backwardsCompatibility: false,
+                  // foregroundColor: appBarForegroundColor.value,
+                  leadingWidth: 56 + appBarMargins,
+                  leading: IconButton(
+                    icon: IconShadowWidget(
+                      Icon(Icons.arrow_back),
+                      showShadow: false,
+                      shadowColor: Colors.black,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  title: Text(
+                    widget.article.title,
+                    overflow: TextOverflow.fade,
+                    style: Theme.of(context).textTheme.headline6.copyWith(
+                        color: Theme.of(context)
+                            .colorScheme
+                            .onSurface
+                            .withOpacity(appBarStateController.value * .87)),
                   ),
                 ),
+                body: Center(child: CircularProgressIndicator()));
+          else
+            return Scaffold(
+              extendBodyBehindAppBar: true,
+              appBar: AppBar(
+                // elevation: appBarStateController.value * 4,
+                shadowColor: Colors.black.withOpacity(
+                    appBarStateController.value * appBarStateController.value),
+                backgroundColor: appBarColor.value,
+                backwardsCompatibility: false,
+                foregroundColor: appBarForegroundColor.value,
+                leadingWidth: 56 + appBarMargins,
+                leading: IconButton(
+                  icon: IconShadowWidget(
+                    Icon(Icons.arrow_back, color: appBarForegroundColor.value),
+                    showShadow: appBarStateController.isDismissed,
+                    shadowColor: Colors.black,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                title: Text(
+                  widget.article.title,
+                  overflow: TextOverflow.fade,
+                  style: Theme.of(context).textTheme.headline6.copyWith(
+                      color: Theme.of(context)
+                          .colorScheme
+                          .onSurface
+                          .withOpacity(appBarStateController.value * .87)),
+                ),
               ),
-              Scrollbar(
-                // thickness: 4,
-                isAlwaysShown: !displayMobileLayout,
-                controller: scrollController,
-                child: SingleChildScrollView(
-                  controller: scrollController,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: margins, right: margins),
-                    child: Column(
-                      children: [
-                        Stack(
-                          alignment: AlignmentDirectional.bottomStart,
-                          children: [
-                            SizedBox(
-                              height: totalSheetHeightDelta,
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                  gradient: RadialGradient(
-                                      center: Alignment.bottomCenter,
-                                      radius: 1,
-                                      colors: <Color>[
-                                    Colors.black,
-                                    Colors.transparent
-                                  ])),
-                              child: Padding(
-                                padding: EdgeInsets.only(
-                                    left: gutters,
-                                    right: gutters,
-                                    bottom: gutters,
-                                    top: gutters * 4),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      widget.article.title,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headline3
-                                          .copyWith(
-                                        color: Colors.white.withOpacity(.87),
-                                        shadows: <Shadow>[
-                                          Shadow(
-                                            blurRadius: 3.0,
-                                            color: Colors.black38,
-                                          ),
-                                          Shadow(
-                                            blurRadius: 8.0,
-                                            color: Colors.black38,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 8,
-                                    ),
-                                    if (widget.article.subtitle != null)
-                                      Text(
-                                        widget.article.subtitle,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .headline6
-                                            .copyWith(
-                                          color: Colors.white.withOpacity(.54),
-                                          shadows: <Shadow>[
-                                            Shadow(
-                                              blurRadius: 3.0,
-                                              color: Colors.black38,
-                                            ),
-                                            Shadow(
-                                              blurRadius: 8.0,
-                                              color: Colors.black38,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                  ],
+              body: NotificationListener(
+                onNotification: (notification) {
+                  if (notification is ScrollUpdateNotification)
+                    setState(() =>
+                        imageScrollPosition -= notification.scrollDelta / 4);
+                },
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: imageScrollPosition,
+                      child: // Container()
+                          ConstrainedBox(
+                        constraints: BoxConstraints(
+                            maxWidth: usefulWidth,
+                            minWidth: usefulWidth,
+                            minHeight: height * 2 / 3,
+                            maxHeight: height),
+                        child: Material(
+                          elevation: 4,
+                          child: Image.network(
+                            widget.article.imageUrl,
+                            fit: BoxFit.cover,
+                            alignment: Alignment.center,
+                            // width: usefulWidth,
+                            // height: height,
+                            semanticLabel: widget.article.altText,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Scrollbar(
+                      // thickness: 4,
+                      isAlwaysShown: !displayMobileLayout,
+                      controller: scrollController,
+                      child: SingleChildScrollView(
+                        controller: scrollController,
+                        child: Padding(
+                          padding:
+                              EdgeInsets.only(left: margins, right: margins),
+                          child: Column(
+                            // verticalDirection: VerticalDirection.up,
+                            children: [
+                              buildTitles(context),
+                              Card(
+                                elevation: 4,
+                                margin: EdgeInsets.only(bottom: gutters),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(
+                                        cardCornerRadius)),
+                                child: Padding(
+                                  padding: EdgeInsets.all(gutters),
+                                  child: Column(
+                                      children: widget.article.buildParagraphs(
+                                          context,
+                                          textAlign: TextAlign.left)),
                                 ),
                               ),
-                            )
-                          ],
-                        ),
-                        Card(
-                          margin: EdgeInsets.zero,
-                          shape: RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.circular(cardCornerRadius)),
-                          child: Padding(
-                            padding: EdgeInsets.all(gutters),
-                            child: Column(children: children2),
+                            ],
                           ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+        });
+  }
+
+  Widget buildTitles(BuildContext context) {
+    var marginLeft = 0.0;
+    if (margins + marginLeft < appBarMargins + 72) {
+      marginLeft = marginLeft + appBarMargins + 72 - (margins + marginLeft);
+    }
+    return PreferredSize(
+      preferredSize: Size.fromHeight(height / 2),
+      // constraints: BoxConstraints(minHeight: height / 2),
+      child: ConstrainedBox(
+        constraints: BoxConstraints(minHeight: height / 2),
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: RadialGradient(
+                  center: Alignment(0.0, 1.2),
+                  radius: 0.8,
+                  colors: <Color>[Colors.black, Colors.transparent])),
+          child: Padding(
+            padding: EdgeInsets.only(
+                bottom: gutters,
+                top: gutters,
+                left: marginLeft,
+                right: marginLeft),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                AutoSizeText(
+                  widget.article.title,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  minFontSize: 10,
+                  style: Theme.of(context).textTheme.headline3.copyWith(
+                    color: Color.alphaBlend(
+                        Colors.white.withOpacity(.87), Colors.black),
+                    shadows: <Shadow>[
+                      Shadow(
+                        blurRadius: 3.0,
+                        color: Colors.black38,
+                      ),
+                      Shadow(
+                        blurRadius: 8.0,
+                        color: Colors.black38,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                if (widget.article.subtitle != null)
+                  AutoSizeText(
+                    widget.article.subtitle,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style: Theme.of(context).textTheme.headline6.copyWith(
+                      color: Color.alphaBlend(
+                          Colors.white.withOpacity(.6), Colors.black),
+                      shadows: <Shadow>[
+                        Shadow(
+                          blurRadius: 3.0,
+                          color: Colors.black38,
+                        ),
+                        Shadow(
+                          blurRadius: 8.0,
+                          color: Colors.black38,
                         ),
                       ],
                     ),
                   ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
